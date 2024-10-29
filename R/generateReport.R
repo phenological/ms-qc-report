2cee4de57111ad0af01fdd95d37b5cbf099a7715
Lucy <lucy@example.com>
2024-10-29 09:48:52 +0800

    First successful reports with tables and plots

--- R/generateReport.R
+++ R/generateReport.R
@@ -5,14 +5,14 @@
 #' @param data The dataset (dataElement) to be included in the report.
 #' @param title The title of the report.
 #' @param output_file The name of the output HTML file.
-#' 
+#' @examples
+#' #generateReport(data = bio, output_file = "~/Desktop/report.html")
 #' @return The path to the generated HTML report.
 #' @export
 
 generateReport <- function(data, title = "QC Report", output_file = "report.html") {
   # Define the path to the Rmd template
-  rmd_file <- system.file("rmarkdown/templates/reportTemplate/reportTemplate.Rmd", package = "ms-qc-report")
-  
+  rmd_file <- system.file("rmarkdown/templates/reportTemplate.Rmd", package = "ms.qc.report")
   # Check if the Rmd template is available
   if (rmd_file == "") {
     stop("Rmd template not found in the package!")
